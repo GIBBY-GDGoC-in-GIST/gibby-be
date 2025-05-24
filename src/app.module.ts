2165afd37d445f6a36159103b3369640a692cceb
GwanLiZa <a56634368@gmail.com>
2025-05-24 16:28:31 +0900

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecruitModule } from './recruit/recruit.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal:true}), RecruitModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
